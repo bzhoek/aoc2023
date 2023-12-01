5c595a68c950eaec424e5b9287cdb0246d77d1ac
Bas van der Hoek <bas@hoek.com>
2023-12-01 21:15:53 +0100

    Solution to part 2 of day 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code in Rust
+
+Solutions per day under `tests/`.
--- tests/day01.rs
+++ tests/day01.rs
@@ -24,4 +24,104 @@ mod tests {
     let total = lines.into_iter().sum::<u32>();
     assert_eq!(53334, total);
   }
+
+  const NUMBER_WORDS: [&str; 9] = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+  ];
+
+  fn convert_words(str: &str) -> Option<u32> {
+    let first = find_first_number(str);
+    let last = find_last_number(str);
+    first.zip(last)
+      .map(|(first, last)| first * 10 + last)
+  }
+
+  #[test]
+  fn test_word_conversion() {
+    assert_eq!(Some(29), convert_words("two1nine"));
+    assert_eq!(Some(42), convert_words("4nineeightseven2"));
+  }
+
+  #[test]
+  fn test_word_finding() {
+    let str = "threefour";
+    let result = NUMBER_WORDS.iter().enumerate()
+      .find(|(index, word)| { str[0..word.len()].eq(**word) });
+    assert_eq!(Some((2usize, &"three")), result)
+  }
+
+  fn is_word<'a>(str: &str) -> Option<(usize, &'a &str)> {
+    NUMBER_WORDS.iter().enumerate()
+      .find(|(index, word)| {
+        str.len() >= word.len() &&
+          str[0..word.len()].eq(**word)
+      })
+  }
+
+  fn find_first_number(str: &str) -> Option<u32> {
+    for (i, char) in str.chars().enumerate() {
+      if char.is_numeric() {
+        return char.to_digit(10);
+      }
+      if let Some((index, number)) = is_word(&str[i..]) {
+        return Some((index + 1) as u32);
+      }
+    }
+    None
+  }
+
+  fn find_last_number(str: &str) -> Option<u32> {
+    for i in (0..str.len()).rev() {
+      if let Some(char) = str.chars().nth(i) {
+        if char.is_numeric() {
+          return char.to_digit(10);
+        }
+      }
+      if let Some((index, number)) = is_word(&str[i..]) {
+        return Some((index + 1) as u32);
+      }
+    }
+    None
+  }
+
+  #[test]
+  fn test_last_number_word() {
+    let str = "twosomefour";
+    let result = find_last_number(str);
+    assert_eq!(Some(4), result)
+  }
+
+  #[test]
+  fn test_first_number_word() {
+    let str = "three";
+    let result = is_word(str);
+    assert_eq!(Some((2usize, &"three")), result)
+  }
+
+  #[test]
+  fn test_later_number_word() {
+    let str = "somethree";
+    let result = find_first_number(str);
+    assert_eq!(Some(3), result)
+  }
+
+  #[test]
+  fn it_solves_part2() {
+    let input = include_str!("day01.input");
+
+    let lines: Vec<_> = input.lines()
+      .flat_map(convert_words)
+      .collect();
+
+    let total = lines.into_iter().sum::<u32>();
+    assert_eq!(52834, total);
+  }
 }
