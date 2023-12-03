90a8ea9b3860a0b15c68c8298a834fa48bf4fe55
Bas van der Hoek <bas@hoek.com>
2023-12-03 22:08:00 +0100

    Solution to part 1 of day 2

--- tests/day02.rs
+++ tests/day02.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+  use std::cmp::max;
   use nom::branch::alt;
   use nom::bytes::complete::tag;
   use nom::character::complete::{one_of, u16};
@@ -47,13 +48,51 @@ mod tests {
   }
 
   #[test]
+  fn test_max_quantities() {
+    let (_, quantities) = parse_line("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+    let green: Vec<_> = quantities.iter()
+      .filter(|(_count, name)| { name.eq(&"green") }).collect();
+    assert_eq!(2, green.len());
+    let (red, green, blue) = max_colors(&quantities);
+    assert_eq!(4, red);
+    assert_eq!(2, green);
+    assert_eq!(6, blue);
+  }
+
+  fn max_colors(quantities: &Vec<(u16, &str)>) -> (u16, u16, u16) {
+    let red = max_color(&quantities, "red");
+    let green = max_color(&quantities, "green");
+    let blue = max_color(&quantities, "blue");
+    (red, green, blue)
+  }
+
+  fn max_color(quantities: &Vec<(u16, &str)>, color: &str) -> u16 {
+    quantities.iter()
+      .filter(|(_count, name)| { name.eq(&color) })
+      .fold(0, |acc, num| max(acc, num.0))
+  }
+
+  #[test]
   fn it_solves_part1() {
     let input = include_str!("day02.input");
 
     let lines: Vec<_> = input.lines()
       .map(parse_line)
+      .map(|(index, quantities)| { (index, max_colors(&quantities)) })
       .collect();
 
     assert_eq!(100, lines.len());
+
+    let possible: Vec<_> = lines.iter()
+      .filter(|(index, (red, green, blue))|
+        { red <= &12u16 && green <= &13u16 && blue <= &14u16 }).collect();
+
+    assert_eq!(51, possible.len());
+
+    let sum: u16 = possible.iter()
+      .map(|(index, _)| index)
+      .sum();
+
+    assert_eq!(2593, sum);
   }
 }
