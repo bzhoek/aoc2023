74863e0cc75da65e066c2610175afc95cdb4d2fd
Bas van der Hoek <bvanderhoek@ripe.net>
2023-12-04 14:53:01 +0100

    Partial solution to day 3 symbol matching

--- tests/day03.rs
+++ tests/day03.rs
@@ -13,41 +13,70 @@ mod tests {
   }
 
   #[test]
-  fn test_nom_parse_separated() {
-    let mut numbers = tuple((
-      take_till(AsChar::is_dec_digit),
-      separated_list1(take_till(AsChar::is_dec_digit), u16::<_, Error<_>>))
-    );
-    let (_rest, (_, result)) = numbers("467..114..").unwrap();
-    assert_eq!(vec!(467, 114), result);
-    let (_rest, (_, result)) = numbers("..35..633.").unwrap();
-    assert_eq!(vec!(35, 633), result);
-  }
-
-  #[test]
   fn test_nom_parse_pairs() {
     let mut numbers =
       many1(pair(take_till(AsChar::is_dec_digit), digit1::<_, Error<_>>)
       );
-    let (_rest, result) = numbers("467..114..").unwrap();
-    assert_eq!(vec!(("", "467"), ("..", "114")), result);
+
     let (_rest, result) = numbers("..35..633.").unwrap();
     assert_eq!(vec!(("..", "35"), ("..", "633")), result);
+
+    let (_rest, result) = numbers("467..114..").unwrap();
+    assert_eq!(vec!(("", "467"), ("..", "114")), result);
+
+    let (digits, symbols) = parse_symbols("...*......");
+    assert_eq!(vec!(3), symbols);
+
+    let matches = match_numbers(result, symbols);
+    assert_eq!(vec!("467"), matches);
+  }
+
+  #[test]
+  fn test_symbol_matching() {
+    let mut numbers =
+      many1(pair(take_till(AsChar::is_dec_digit), digit1::<_, Error<_>>)
+      );
+
+    let (_rest, result) = numbers("......755.").unwrap();
+    assert_eq!(vec!(("......", "755")), result);
+
+    let (digits, symbols) = parse_symbols("...$.*....");
+    assert_eq!(vec!(3, 5), symbols);
+
+    let matches = match_numbers(result, symbols);
+    assert_eq!(vec!("755"), matches);
+  }
+
+  fn match_numbers<'a>(result: Vec<(&'a str, &'a str)>, symbols: Vec<usize>) -> Vec<&'a str> {
+    let mut matches: Vec<&str> = vec![];
+    let mut offset = 0;
+    for (prefix, number) in result.iter() {
+      let start = offset + prefix.len();
+      let end = start + number.len();
+      if let Some(index) = symbols.iter().find(|index| {
+        println!("{} {} {}", start, index, end);
+        *index + 1 >= start && index <= &&end
+      }) {
+        matches.push(number);
+      };
+      offset = end;
+    }
+    matches
   }
 
   #[test]
   fn test_parse_chars() {
     let subject = "467..114..";
-    let (digits, symbols) = parse_line(subject);
+    let (digits, symbols) = parse_symbols(subject);
     assert_eq!(vec!(0, 1, 2, 5, 6, 7), digits);
     assert_eq!(Vec::<usize>::new(), symbols);
     let subject = "617*......";
-    let (digits, symbols) = parse_line(subject);
+    let (digits, symbols) = parse_symbols(subject);
     assert_eq!(vec!(0, 1, 2), digits);
     assert_eq!(vec!(3), symbols);
   }
 
-  fn parse_line(subject: &str) -> (Vec<usize>, Vec<usize>) {
+  fn parse_symbols(subject: &str) -> (Vec<usize>, Vec<usize>) {
     let mut digits: Vec<usize> = vec![];
     let mut symbols: Vec<usize> = vec![];
     for (i, char) in subject.chars().enumerate() {
