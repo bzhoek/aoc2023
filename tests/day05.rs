f881aeda0d1229bf96364c388ec68a013c90d06c
Bas van der Hoek <bas@hoek.com>
2023-12-05 14:40:41 +0100

    Solve part 1 of day 5

--- tests/day05.rs
+++ tests/day05.rs
@@ -14,7 +14,7 @@ mod tests {
   fn test_parse_sample() {
     let input = include_str!("day05.sample");
     let (input, (_, _, seeds)) = tuple((
-      tag("seeds:"), multispace1, separated_list1(multispace1, complete::u16::<_, Error<_>>))
+      tag("seeds:"), multispace1, separated_list1(multispace1, complete::u64::<_, Error<_>>))
     )(input).unwrap();
     let (input, seed_to_soil) = parse_ranges(input, "seed-to-soil map:");
     let (input, soil_to_fertilizer) = parse_ranges(input, "soil-to-fertilizer map:");
@@ -37,9 +37,9 @@ mod tests {
 
     let seed = seeds.first().unwrap();
     assert_eq!(81, map_through(*seed, &seed_to_soil));
-    assert_eq!(14, map_through(14u16, &seed_to_soil));
-    assert_eq!(57, map_through(55u16, &seed_to_soil));
-    assert_eq!(13, map_through(13u16, &seed_to_soil));
+    assert_eq!(14, map_through(14, &seed_to_soil));
+    assert_eq!(57, map_through(55, &seed_to_soil));
+    assert_eq!(13, map_through(13, &seed_to_soil));
 
     let locations: Vec<_> = seeds.iter()
       .map(|seed| map_through(*seed, &*seed_to_soil))
@@ -55,13 +55,44 @@ mod tests {
     assert_eq!(Some(35), result);
   }
 
-  fn parse_ranges<'a>(input: &'a str, heading: &str) -> (&'a str, Vec<(u16, Range<u16>)>) {
+  #[test]
+  fn is_solves_part1() {
+    let input = include_str!("day05.input");
+    let (input, (_, _, seeds)) = tuple((
+      tag("seeds:"), multispace1, separated_list1(multispace1, complete::u64::<_, Error<_>>))
+    )(input).unwrap();
+    let (input, seed_to_soil) = parse_ranges(input, "seed-to-soil map:");
+    let (input, soil_to_fertilizer) = parse_ranges(input, "soil-to-fertilizer map:");
+    let (input, fertilizer_to_water) = parse_ranges(input, "fertilizer-to-water map:");
+    let (input, water_to_light) = parse_ranges(input, "water-to-light map:");
+    let (input, light_to_temperature) = parse_ranges(input, "light-to-temperature map:");
+    let (input, temperature_to_humidity) = parse_ranges(input, "temperature-to-humidity map:");
+    let (_, humidity_to_location) = parse_ranges(input, "humidity-to-location map:");
+
+    assert_eq!(20, seeds.len());
+    assert_eq!(14, seed_to_soil.len());
+
+    let locations: Vec<_> = seeds.iter()
+      .map(|seed| map_through(*seed, &*seed_to_soil))
+      .map(|seed| map_through(seed, &*soil_to_fertilizer))
+      .map(|seed| map_through(seed, &*fertilizer_to_water))
+      .map(|seed| map_through(seed, &*water_to_light))
+      .map(|seed| map_through(seed, &*light_to_temperature))
+      .map(|seed| map_through(seed, &*temperature_to_humidity))
+      .map(|seed| map_through(seed, &*humidity_to_location))
+      .collect();
+
+    let result = locations.iter().min().copied();
+    assert_eq!(Some(111627841), result);
+  }
+
+  fn parse_ranges<'a>(input: &'a str, heading: &str) -> (&'a str, Vec<(u64, Range<u64>)>) {
     let (input, (_, _, _, vector)) = tuple((
       multispace1,
       tag(heading),
       multispace1,
       map(
-        separated_list1(multispace1, complete::u16::<_, Error<_>>), |list| {
+        separated_list1(multispace1, complete::u64::<_, Error<_>>), |list| {
           list.chunks(3).map(|chunk| {
             (chunk[0], Range { start: chunk[1], end: chunk[1] + chunk[2] })
           }).collect::<Vec<_>>()
@@ -71,7 +102,7 @@ mod tests {
     (input, vector)
   }
 
-  fn map_through(seed: u16, s2s: &[(u16, Range<u16>)]) -> u16 {
+  fn map_through(seed: u64, s2s: &[(u64, Range<u64>)]) -> u64 {
     let soil = s2s.iter()
       .find(|(_destination, range)| {
         range.contains(&seed)
